722e71d18f7493a87393d138bdb53b09258d96f6
bard <cyborgaeon@gmail.com>
2021-11-09 23:45:35 +0000

    for debugging sake I've added a swagger implementation

---
+++ ApiIntegration.Swagger/Controllers/OverrideAppEntryController.cs
@@ -0,0 +1,29 @@
+﻿using ApiIntegration.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiIntegration.Swagger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OverrideAppEntryController : ControllerBase
+    {
+        private readonly ILogger<OverrideAppEntryController> _logger;
+        private readonly IImporter importer;
+        public OverrideAppEntryController(ILogger<OverrideAppEntryController> logger, IImporter importerInstance)
+        {
+            this.importer = importerInstance;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task RunApplication()
+        {
+            await this.importer.Execute(1);
+        }
+    }
+}
---
+++ ApiIntegration/ApiDownloader.cs
@@ -0,0 +1,17 @@
+﻿using ApiIntegration.Interfaces;
+using ApiIntegration.ProviderModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiIntegration
+{
+    public class ApiDownloader : IApiDownloader
+    {
+        public Task<ApiAvailabilityResponse> Download()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

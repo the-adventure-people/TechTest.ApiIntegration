684694c3b9dc99a7527e56cd916fbdc6f4d7261c
vulcanis-dev <silverstriker49@gmail.com>
2022-02-27 13:40:01 +0000

    Implemented IApiDownloader test proj wouldnt compile unless updated to 3.1 Target data 'Price' was decimal not int hence model change

---
+++ ApiIntegration.Tests/ApiIntegrationImporterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiIntegration.Tests
+{
+    [TestFixture]
+    public class ApiIntegrationImporterTests
+    {
+        private Importer _importer;
+        private TourRepository _tourRepository;
+        private ProviderRepository _providerRepository;
+        private ApiDownloader _apiDownloader;
+        private ILogger<ConsoleLoggerProvider> _logger;
+
+        
+
+        [SetUp]
+        public void SetUp()
+        {
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            _logger = loggerFactory.CreateLogger<ConsoleLoggerProvider>();
+            _tourRepository = new TourRepository();
+            _providerRepository = new ProviderRepository();
+            _apiDownloader = new ApiDownloader();           
+            _importer = new Importer(_tourRepository, _providerRepository, _apiDownloader, _logger);
+        }
+
+        [Test]
+        public async Task CanDownload()
+        {
+            await _importer.Execute(1);
+        }
+    }
+}
---
+++ ApiIntegration/ApiDownloader.cs
@@ -0,0 +1,33 @@
+﻿using ApiIntegration.Interfaces;
+using ApiIntegration.ProviderModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiIntegration
+{
+    public class ApiDownloader : IApiDownloader
+    {
+        private HttpClient _httpClient;
+        private string _url = "http://tap.techtest.s3-website.eu-west-2.amazonaws.com/"; 
+
+        public ApiDownloader()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(_url)                
+            };
+        }
+
+        public async Task<ApiAvailabilityResponse> Download()
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, "/");
+            var response = await _httpClient.SendAsync(httpRequest);
+            var availabilityResponse = JsonConvert.DeserializeObject<ApiAvailabilityResponse>(await response.Content.ReadAsStringAsync());
+            return availabilityResponse;
+        }
+    }
+}
